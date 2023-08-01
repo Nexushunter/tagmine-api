2b41d1e15a6c92e85097a42f41438c7006427a5e
Nexushunter <nexus@nexushunter.dev>
2023-07-31 23:13:20 -0400

    Add dart client and oas spec

---
+++ lib/tagmine_api.dart
@@ -0,0 +1,19 @@
+import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
+
+@Openapi(
+  additionalProperties: DioProperties(
+      pubName: 'tagmine_api_client',
+      pubDescription: 'OAS generated dart api for tagmine.ca',
+      useEnumExtension: true,
+      sortParamsByRequiredFlag: true,
+      sortModelPropertiesByRequiredFlag: true,
+      nullableFields: false
+  ),
+  inputSpecFile: './openapi.yaml',
+  generatorName: Generator.dio,
+  outputDirectory: 'clients/dart',
+  overwriteExistingFiles: false,
+  alwaysRun: true,
+  fetchDependencies: true,
+)
+class TagmineClientConfig extends OpenapiGeneratorConfig {}
